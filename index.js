a4fbf8803a8590e84783a661f19720c8360dcb49
naga <nagasatish542gmail.com>
2021-09-06 16:44:53 +0000

    100

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello AWS and Devops Architecture programme course| Duration -$% days !\n'
+  const msg = 'Hello    jenkins-- AWS and Devops Architecture programme course| Duration -$% days !\n'
   res.end(msg);
 });
 
