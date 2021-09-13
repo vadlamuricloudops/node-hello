c98511c29560b2543c1534650647181ba29a2000
vadlamuricloudops <88264138+vadlamuricloudops@users.noreply.github.com>
2021-09-13 21:15:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
 
-  const msg = 'Hello    jenkins-- AWS and Devops Architecture programme course| Duration -$% days !\n'
+  const msg = 'Hello    jenkins !\n'
   res.end(msg);
 });
 
