ad457ac7372a53e0954666c972ac1d27ab8d9735
vadlamuricloudops <88264138+vadlamuricloudops@users.noreply.github.com>
2021-09-13 22:07:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
 
-  const msg = 'Hello  arun and ravi  jenkins !\n'
+  const msg = 'Hello  arun and ravi  jenkins DEVOOPS!\n'
   res.end(msg);
 });
 
