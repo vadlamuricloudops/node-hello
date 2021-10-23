9aafb391fbddc2aa14288fc281ee58b1c31134c5
satish <vadlamuricloudops@gmail.com>
2021-10-23 15:22:04 +0000

    5

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
 
-  const msg = 'Hello this is aws and devops \n'
+  const msg = 'Hello this is aws and devops sessions ! \n'
   res.end(msg);
 });
 
